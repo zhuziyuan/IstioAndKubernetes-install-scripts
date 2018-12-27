e72dec8ef69cfeda6d1ba2f3fd078fd95f46abe3
李远君 <1123431949@qq.com>
2018-12-27 10:45:40 +0800

    Update install_istio_on_k8s.sh

--- install_istio_on_k8s.sh
+++ install_istio_on_k8s.sh
@@ -7,14 +7,20 @@ echo "
 Istio 对 Pod 和服务的要求
 要成为服务网格的一部分，Kubernetes 集群中的 Pod 和服务必须满足以下几个要求：
 
-1. 需要给端口正确命名：服务端口必须进行命名。端口名称只允许是<协议>[-<后缀>-]模式，其中<协议>部分可选择范围包括 http、http2、grpc、mongo 以及 redis，Istio 可以通过对这>些协议的支持来提供路由能力。例如 name: http2-foo 和 name: http 都是有效的端口名，但 name: http2foo 就是无效的。如果没有给端口进行命名，或者命名没有使用指定前缀，那么这>一端口的流量就会被视为普通 TCP 流量（除非显式的用 Protocol: UDP 声明该端口是 UDP 端口）。
+1. 需要给端口正确命名：服务端口必须进行命名。端口名称只允许是<协议>[-<后缀>-]模式，其中<协议>部分可选择范围包括
+http、http2、grpc、mongo 以及 redis，Istio 可以通过对这>些协议的支持来提供路由能力。
+例如 name: http2-foo 和 name: http 都是有效的端口名，但 name: http2foo 就是无效的。
+如果没有给端口进行命名，或者命名没有使用指定前缀，那么这一端口的流量就会被视为普通 TCP 流量（除非显式的用 Protocol: UDP 声明该端口是 UDP 端口）。
 
 2. 关联服务：Pod 必须关联到 Kubernetes 服务，如果一个 Pod 属于多个服务，这些服务不能再同一端口上使用不同协议，例如 HTTP 和 TCP。
 
-3. Deployment 应带有 app 以及 version 标签：在使用 Kubernetes Deployment 进行 Pod 部署的时候，建议显式的为 Deployment 加上 app 以及 version 标签。每个 Deployment 都应该
-有一个有意义的 app 标签和一个用于标识 Deployment 版本的 version 标签。app 标签在分布式跟踪的过程中会被用来加入上下文信息。Istio 还会用 app 和 version 标签来给遥测指标数
-据加入上下文信息。
+3. Deployment 应带有 app 以及 version 标签：在使用 Kubernetes Deployment 进行 Pod 部署的时候，
+建议显式的为 Deployment 加上 app 以及 version 标签。每个 Deployment 都应该有一个有意义的 app 标签和一个用于标识 Deployment 版本的 version 标签。
+app 标签在分布式跟踪的过程中会被用来加入上下文信息。Istio 还会用 app 和 version 标签来给遥测指标数据加入上下文信息。
 "
+
+ISTIO_HOME=/usr/local/istio
+HELM_HOME=/usr/local/helm
 
 istioctl version > /dev/null 2>&1
 
@@ -64,19 +70,14 @@ EOF
 
     chmod +x downloadIstioCandidate.sh
     sh downloadIstioCandidate.sh
-    mv istio-* /usr/local/
-    cd /usr/local/istio*
-
-    ISTIO_HOME=`pwd`
+    mv istio* /usr/local/istio
 
     cat << EOF >> $HOME/.bashrc
 
 # add istio path
 export ISTIO_PATH=$ISTIO_HOME
-export PATH=$ISTIO_HOME/bin:$PATH
+export PATH=\$ISTIO_HOME/bin:\$PATH
 EOF
-
-    refresh_env=`source $HOME/.bashrc`
 
     # version
     rm -f downloadIstioCandidate.sh
@@ -87,13 +88,12 @@ EOF
     sleep 1
 else
     echo "************* Istio already exists **********************"
-    echo "ISTIO INSTALL PATH: "$ISTIO_PATH
-    istioctl version
+    echo "ISTIO INSTALL PATH: "$ISTIO_HOME
+    $ISTIO_HOME/bin/istioctl version
     echo "*********************************************************"
     sleep 1
 fi
 
-ISTIO_HOME="$ISTIO_PATH"
 # download helm
 helm version > /dev/null 2>&1
 
@@ -103,27 +103,27 @@ if [ $? -eq 127 ];then
     cat << EOF >> $HOME/.bashrc
 
 # add helm path
-export PATH=/usr/local/helm:$PATH
+export PATH=/usr/local/helm:\$PATH
 EOF
 
-    refresh_env=`source $HOME/.bashrc`
     echo -e "\n************* Helm the default installation location **********************"
-    echo "HELM INSTALL PATH: /usr/local/helm"
+    echo "HELM INSTALL PATH: "$HELM_HOME
     helm version
     echo -e "\n***************************************************************************"
     sleep 1
 else
     echo -e "\n************* Helm  already  exists **********************"
-    echo "HELM INSTALL PATH: /usr/local/helm"
+    echo "HELM INSTALL PATH: "$HELM_HOME
     helm version
     echo -e "\n**********************************************************"
     sleep 1
 fi
 
 
-helm version > /dev/null 2>&1
+$HELM_HOME/helm version > /dev/null 2>&1
 if [ $? -eq 127 ];then 
-    echo '环境变量刷新失败,请执行"source $HOME/.bashrc"命令来刷新环境变量,然后重新执行该脚本即可.'
+    echo '安装失败, 重新执行该脚本即可.'
+    exit
 fi
 #echo "安装 Istio 的自定义资源定义（CRD）"
 #kubectl apply -f $ISTIO_PATH/install/kubernetes/helm/istio/templates/crds.yaml
@@ -134,7 +134,20 @@ kubectl apply -f $ISTIO_HOME/install/kubernetes/helm/helm-service-account.yaml
 sleep 1
 
 echo -e  "\n使用 service account 在kubernetes集群中安装 Tiller (helm server)."
-helm init --service-account tiller
+$HELM_HOME/helm init --service-account tiller
+
+while true
+do
+    echo "check helm server install status..."
+    sleep 5
+    tillerNum=`kubectl get po -n kube-system -l name=tiller | grep tiller| wc -l`
+    if [ $tillerNum -ge 1 ];
+    then
+       break
+    fi
+done
+
+echo "helm server created successfully."
 sleep 1
 
 echo -e "\n安装 Istio"
@@ -146,5 +159,14 @@ echo -e "\n安装 Istio"
 # 如果想启用全局双向 TLS，请将 global.mtls.enabled 设置为 true
 # helm install install/kubernetes/helm/istio --name istio --namespace istio-system --set global.mtls.enabled=true
 
-# 默认情况下，Istio 使用 负载均衡器 服务对象类型。有些平台不支持 负载均衡器 服务对象类型。对于缺少 负载均衡器 支持的平台，安装需要带有 “NodePort” 支持的 Istio
-helm install $ISTIO_HOME/install/kubernetes/helm/istio --name istio --namespace istio-system --set gateways.istio-ingressgateway.type=NodePort --set gateways.istio-egressgateway.type=NodePort
+# 默认情况下，Istio 使用 负载均衡器 服务对象类型。有些平台不支持 负载均衡器 服务对象类型。
+# 对于缺少 负载均衡器 支持的平台，安装需要带有 “NodePort” 支持的 Istio
+$HELM_HOME/helm install $ISTIO_HOME/install/kubernetes/helm/istio \
+     --name istio \
+     --namespace istio-system \
+     --set gateways.istio-ingressgateway.type=NodePort \
+     --set gateways.istio-egressgateway.type=NodePort
+
+# 刷新环境变量
+source ~/.bashrc > /dev/null 2>&1
+echo "install istio ok!"
