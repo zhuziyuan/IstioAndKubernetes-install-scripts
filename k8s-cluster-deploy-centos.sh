65a63c9bb71bcc3bef3d9703827b6c37c3619b26
tomoncle <1123431949@qq.com>
2019-02-26 11:45:19 +0800

    .

--- k8s-cluster-deploy-centos.sh
+++ k8s-cluster-deploy-centos.sh
@@ -8,25 +8,25 @@ yum install epel-release -y && \
   pip install --no-cache-dir ansible -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com && \
   git clone https://github.com/tomoncle/kubeasz.git && \
   mkdir -p /etc/ansible && \
-  mv kubeasz/* /etc/ansible -f
+  mv kubeasz/* /etc/ansible -f \
+  wget https://www.tomoncle.com/download/kubernetes/k8s.1-10-6.tar.gz \
+  tar zxvf k8s.1-10-6.tar.gz \
+  mv bin/* /etc/ansible/bin -f 
 
 echo "config success!!!"
 echo "
 
-++++++++++++++++++++++++++++++++++++++++++++++++++++
-             ******   NETX  ******
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+             ******   NETX configuration   ******
 
-1.download k8s.*.tar.gz
-  $ tar zxvf k8s.*.tar.gz
-  $ mv bin/* /etc/ansible/bin -f
-
-2.configuration
-  $ ssh-keygen -t rsa -b 2048
+* $ ssh-keygen -t rsa -b 2048
   $ ssh-copy-id -i /root/.ssh/id_rsa.pub root@192.168.137.142
 
-  $ cd /etc/ansible && cp example/hosts.m-masters.example hosts
+
+* $ cd /etc/ansible && cp example/hosts.m-masters.example hosts
   $ ansible all -m ping 
   $ ansible-playbook 90.setup.yml
-++++++++++++++++++++++++++++++++++++++++++++++++++++
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 "
